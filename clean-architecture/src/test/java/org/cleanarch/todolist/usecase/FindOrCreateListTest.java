97c410d9ee0c4c68cf2eb8eb4ae6db083b6dd775
Angel Rojo Marijuan <rojo.angel@gmail.com>
2021-08-15 18:48:18 +0200

    should return list when found

--- clean-architecture/src/test/java/org/cleanarch/todolist/usecase/FindOrCreateListTest.java
+++ clean-architecture/src/test/java/org/cleanarch/todolist/usecase/FindOrCreateListTest.java
@@ -8,11 +8,14 @@ import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
 import java.util.Collections;
+import java.util.List;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
 import static org.hamcrest.core.IsEqual.equalTo;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 class FindOrCreateListTest {
@@ -23,9 +26,22 @@ class FindOrCreateListTest {
     @Test
     void should_create_list_when_not_found() {
         MockitoAnnotations.openMocks(this);
-        when(listStore.findAll()).thenReturn(Collections.emptyList());
         var todoList = new TodoList();
+        when(listStore.findAll()).thenReturn(Collections.emptyList());
         when(listStore.save(any(TodoList.class))).thenReturn(todoList);
+
+        FindOrCreateList findOrCreateList = new FindOrCreateList(listStore);
+        var findOrCreateListResponse = findOrCreateList.apply(new FindOrCreateListRequest());
+
+        assertThat(findOrCreateListResponse.getTodoListId(), is(equalTo(todoList.getId().getUuid())));
+    }
+
+    @Test
+    void should_return_list_when_found() {
+        MockitoAnnotations.openMocks(this);
+        var todoList = new TodoList();
+        when(listStore.findAll()).thenReturn(List.of(todoList));
+        verify(listStore, never()).save(any(TodoList.class));
 
         FindOrCreateList findOrCreateList = new FindOrCreateList(listStore);
         var findOrCreateListResponse = findOrCreateList.apply(new FindOrCreateListRequest());
